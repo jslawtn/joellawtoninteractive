2c607c74aaddb946ab253215a489807f4c216643
Joel Lawton <Joel.Lawton@interact-intranet.com>
2019-08-23 15:26:46 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <checklist></checklist>
+  </div>
+</template>
+
+<script>
+import checklist from './components/CheckList.vue';
+export default {
+  name: 'app',
+  components: {
+    checklist
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CheckList.vue
@@ -0,0 +1,58 @@
+<style>
+ .task-marked{
+   
+ }
+</style>
+
+<template>
+  <div>
+    <div v-for="(item, index) in checklist" :key="index">
+        <input type="checkbox" :checked="item.checked" v-model="item.checked"/>
+        <p>{{item.description}}</p>
+        <h6>{{formatDateTime(item.created)}}</h6>
+    </div>
+    <input type="input" placeholder="Say Something..." v-model="itemDescription" @keyup.enter="submit"/>
+    <button v-on:click="submit" :disabled="itemDescription === ''">Add</button>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+import { formatDateTime } from '../services/dateTime.js';
+
+export default {
+  name:'check-list',
+  data(){
+    return{
+      itemDescription: '',
+      checklist:[]
+    }
+  },
+  created: function(){
+    this.checklist = JSON.parse(localStorage.getItem('check-list'));
+
+    if(this.checklist === null){
+      this.checklist = [];
+    }
+  },
+  methods:{
+    formatDateTime,
+    submit(){
+      const checklistItem = {
+        description: this.itemDescription,
+        checked: false,
+        created: moment.utc()
+      }
+
+      this.checklist.push(checklistItem);
+      this.itemDescription = '';
+    }
+  },
+  watch:{
+    checklist: function(){
+      localStorage.setItem('check-list', JSON.stringify(this.checklist));
+    },
+    deep: true
+  }
+}
+</script>
